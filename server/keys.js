fff137cd0a7ca2760789158a2b4e47015b055d49
Dimpker <dimpker@Andi.local>
2020-02-03 18:39:47 +0100

    Mongoose set up

---
+++ server/keys.js
@@ -0,0 +1,4 @@
+  module.exports = {
+    mongoURI:
+      'mongodb+srv://andidollar2:9zlhBB0SEL4XBvTI@cluster0-lotti.mongodb.net/data?retryWrites=true&w=majority'
+  };
--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ const app = express();
 const port = process.env.PORT || 5000;
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const db = require('./keys').mongoURI;
+const mongoose = require("mongoose");
 
 app.use(bodyParser.json());
 app.use(
@@ -14,4 +16,11 @@ app.use(cors());
 app.listen(port, () => {
   console.log("Server is running on " + port + "port");
 });
+
+app.use('/cities', require('./routes/cities'));
+
+mongoose.connect(db, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true })
+    .then(() => console.log('Connection to Mongo DB established'))
+    .catch(err => console.log(err));
+
 
