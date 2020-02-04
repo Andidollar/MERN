f090ec233e68e34b22544ab636d2ac0b42883a59
Dimpker <dimpker@Andi.local>
2020-02-04 19:59:09 +0100

    Feb 4 MongoDB retrieved

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,51 @@
 import React, { Component } from 'react';
-import Landing from './components/Landing';
+// import Landing from './components/Landing';
 import Cities from './components/Cities';
 // import 'bootstrap/dist/css/bootstrap.min.css';
 // import $ from 'jquery';
 // import Popper from 'popper.js';
 // import 'bootstrap/dist/js/bootstrap.bundle.min';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-export default class App extends Component {
+// import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+class App extends Component {
   render() {
     return (
-      <BrowserRouter>
-        <div className="App">
-          <Switch>
-            <Route exact path='/' component={Landing} />
-          </Switch>
-          <Switch>
-            <Route exact path='/' component={Cities} />
-          </Switch>
-        </div>
-      </BrowserRouter>
-    );
-  }
+      <Cities cities={this.state.cities} />
+        )
+    }
+
+    state = {
+        cities: []
+    };
+
+    componentDidMount() {
+        fetch('http://localhost:5000/cities/all')
+            .then(res => res.json())
+            .then((data) => {
+              console.log(data)
+                this.setState({ cities: data })
+            })
+            .catch(console.log)
+    }
 }
+
+export default App
+
+
+// class App extends Component {
+//   render() {
+//     return (
+//       <BrowserRouter>
+//         <div className="App">
+//           <Switch>
+//             <Route exact path='/' component={ Landing } />
+//           </Switch>
+//           <Switch>
+//             <Route exact path='/' component={ Cities } />
+//           </Switch>
+//         </div>
+//       </BrowserRouter>
+//     );
+//   }
+// }
--- client/src/components/Cities.js
+++ client/src/components/Cities.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import '../index.css';
-// import 'bootstrap/dist/css/bootstrap.min.css';
-// import 'bootstrap/dist/js/bootstrap.bundle.min';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 
-const Cities = () => {
-return (<div><h2>Connect</h2></div>)
+const Cities = ({cities}) => {
+    return (
+        <div>
+            {cities.map((city) => (
+                <div key={city._id}>
+                    <h2>{city.name}</h2>
+                    <p>{city.country}</p>
+                    <img
+                        src={city.image}
+                        alt="pic"
+                        style={{
+                        width: 400,
+                        height: 300,
+                        paddingLeft: 15
+                    }}/>
+                </div>
+            ))}</div>
+    )
 };
-
-
 
 export default Cities
